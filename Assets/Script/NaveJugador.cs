6e838f83de26f5ccdfd73ae91ad5b87d4d2b65df
Biel Pérez de Tudela Cañete <bielperezdetudela@ceroca.cat>
2024-09-19 19:28:56 +0200

    Creacion de bordes pantalla

--- Assets/Script/NaveJugador.cs
+++ Assets/Script/NaveJugador.cs
@@ -6,10 +6,21 @@ public class NaveJugador : MonoBehaviour
 {
     private float _vel;
 
+    private Vector2 minPantalla, maxPantalla;
+
     // Start is called before the first frame update
     void Start()
     {
         _vel = 8;
+    
+        minPantalla = Camera.main.ViewportToWorldPoint(new Vector2(0,0));
+        maxPantalla = Camera.main.ViewportToWorldPoint(new Vector2(1,1));
+
+        minPantalla.x = minPantalla.x + 0.75f;
+        maxPantalla.x = maxPantalla.x - 0.75f;
+        minPantalla.y = minPantalla.y + 0.75f;
+        maxPantalla.y = maxPantalla.y - 0.75f;
+
     }
 
     // Update is called once per frame
@@ -23,6 +34,12 @@ public class NaveJugador : MonoBehaviour
 
         Vector2 novaPos = transform.position;
         novaPos = novaPos + direccioindicada * _vel * Time.deltaTime;
+
+        novaPos.x = Mathf.Clamp(novaPos.x, minPantalla.x, maxPantalla.x);
+        novaPos.y = Mathf.Clamp(novaPos.y, minPantalla.y, maxPantalla.y);
+
         transform.position = novaPos;
+    
+    
     }
 }
