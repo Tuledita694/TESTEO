1049e138b3c1b55c5ea03c6b31dcb6545ece60fa
Biel Pérez de Tudela Cañete <bielperezdetudela@ceroca.cat>
2024-09-26 17:01:51 +0200

    Medidas pantallas amb nau

--- Assets/Script/NaveJugador.cs
+++ Assets/Script/NaveJugador.cs
@@ -15,11 +15,14 @@ public class NaveJugador : MonoBehaviour
     
         minPantalla = Camera.main.ViewportToWorldPoint(new Vector2(0,0));
         maxPantalla = Camera.main.ViewportToWorldPoint(new Vector2(1,1));
-
-        minPantalla.x = minPantalla.x + 0.75f;
-        maxPantalla.x = maxPantalla.x - 0.75f;
-        minPantalla.y = minPantalla.y + 0.75f;
-        maxPantalla.y = maxPantalla.y - 0.75f;
+        float midaMeitatImatgeX = GetComponent<SpriteRenderer>().sprite.bounds.size.x * transform.localScale.x / 2;
+        float midaMeitatImatgeY = GetComponent<SpriteRenderer>().bounds.size.y / 2;
+        //minPantalla.x = minPantalla.x + 0.75f;
+        //minPantalla.x += 0.75f;
+        minPantalla.x += midaMeitatImatgeX;
+        maxPantalla.x -= midaMeitatImatgeX;
+        minPantalla.y += midaMeitatImatgeY;
+        maxPantalla.y -= midaMeitatImatgeY;
 
     }
 
